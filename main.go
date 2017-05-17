33fb12f40a920adbeb88f926cc29166a4ae4a693
Felix Sun <sunfmin@gmail.com>
2017-05-17 17:17:52 +0800

    remove gzip compress, since it should be done in middleware

--- main.go
+++ main.go
@@ -99,7 +99,6 @@ func printserver(dir string, apiset *parser.APISet, apipkg string, impl string) 
 		"time",
 		"io",
 		"strings",
-		"compress/gzip",
 		"encoding/json",
 		"encoding/base64",
 		apipkg,
